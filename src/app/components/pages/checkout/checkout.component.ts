344fd8c31bb61d4ebe3d611127915358c3e17bdb
khadyKebe <khadyyaram@gmail.com>
2021-01-21 21:59:24 +0100

    change user info form

--- src/app/components/pages/checkout/checkout.component.ts
+++ src/app/components/pages/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { CartService } from '../../shared/services/cart.service';
 import { Observable, of } from 'rxjs';
 import { CartItem } from 'src/app/modals/cart-item';
 import { ProductService } from '../../shared/services/product.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -13,6 +14,7 @@ export class CheckoutComponent implements OnInit {
 
   public cartItems: Observable<CartItem[]> = of([]);
   public buyProducts: CartItem[] = [];
+  public userInfoForm: FormGroup;
 
   amount: number;
   payments: string[] = ['Create an Account?', 'Flat Rate'];
@@ -23,11 +25,25 @@ export class CheckoutComponent implements OnInit {
   constructor(private cartService: CartService, public productService: ProductService) { }
 
   ngOnInit() {
+    this.initForm();
     this.cartItems = this.cartService.getItems();
     this.cartItems.subscribe(products => this.buyProducts = products);
     this.getTotal().subscribe(amount => this.amount = amount);
   }
 
+  initForm(){
+    this.userInfoForm = new FormGroup(
+      {
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        address: new FormControl('', Validators.required),
+        city: new FormControl('', Validators.required),
+        phone: new FormControl('', Validators.required)
+
+});
+}
+  public sendOrderInfo(){}
   public getTotal(): Observable<number> {
     return this.cartService.getTotalAmount();
     }
