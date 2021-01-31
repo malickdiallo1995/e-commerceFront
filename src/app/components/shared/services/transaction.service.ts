99ff0ed08f262394c498d9ea59a1b13e9e4a965b
khadyKebe <khadyyaram@gmail.com>
2021-01-31 18:45:47 +0100

    payment

---
+++ src/app/components/shared/services/transaction.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Transaction} from '../../../modals/transaction.model';
+import {environment} from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  private url = environment.base_url+'/reglements/transaction/';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  public getTransactions():Observable<Transaction[]>{
+    return this.httpClient.get<Transaction[]>(this.url);
+  }
+
+  public getTrannsactionById(id: number):Observable<Transaction[]>{
+    return this.httpClient.get<Transaction[]>(this.url+""+id);
+  }
+
+
+  public addTransaction(transaction: Transaction):Observable<Transaction>{
+    return this.httpClient.post(this.url+"create/bis", transaction);
+  }
+}
